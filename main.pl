6518d75f24dd93ee8d37b278312731a8a8bc3c64
slepokurovamf <slepokurovamf@oooinex.ru>
2022-10-05 15:35:53 +0300

    add new operations

--- main.pl
+++ main.pl
@@ -16,6 +16,20 @@ remove(L, [], L) :- !.
 remove([X|Tail], [X|Rest], Result) :- !, remove(Tail, Rest, Result).
 remove([X|Tail], L, [X|Result]):- remove(Tail, L, Result).
 
+range_list(X,X,[X]) :- !.
+range_list(Left, Right, [Left|Rest]) :-
+    Left =< Right,
+    NewLeft is Left+1,
+    range_list(NewLeft, Right, Rest).
+
+alleq(Target, Source) :- 
+    forall(member(X, Target), member(X, Source)).
+
+allneq(Target, Source) :-
+    length(Target, TLength),
+    length(Source, SLength),
+    (TLength \= 0 ; TLength \= SLength),
+    forall(member(X, Target), not(member(X, Source))).
 
 :- dynamic level/1.
 level(1).
@@ -30,6 +44,18 @@ pop_level :-
     retract(level(Current)),
     asserta(level(Prev)).
 
+:- dynamic neqv/3.
+:- dynamic eqv/3.
+
+create_neqv(Level, LName, RName) :- asserta(neqv(Level, LName, RName)).
+delete_neqv(Level, LName, RName) :- (retract(neqv(Level, LName, RName)) ; true).
+delete_neqvs(Level) :-
+    (retractall(neqv(Level, _LName, _RName)) ; true).
+
+create_eqv(Level, LName, RName) :- asserta(eqv(Level, LName, RName)).
+delete_eqv(Level, LName, RName) :- (retract(eqv(Level, LName, RName)) ; true).
+delete_eqvs(Level) :-
+    (retractall(eqv(Level, _LName, _RName)) ; true).
 
 % traverse(Level, node(Vertex, Adjacency))
 traverse(Vertex) :-
@@ -50,7 +76,6 @@ traverse(node(Vertex, [T, F])) :-
    		;	writeln("UNREACHABLE"),
     		node(F, FList), traverse(node(F, FList))
     ).
-
 
 :- dynamic var/6.
 create_var(Level, Name, Left, Right, Possible, Impossible) :-
@@ -107,7 +132,7 @@ router(Vertex) :-
     Operation = "EQ",
     level(Level), var(Level, Name, Left, Right, Possible, Impossible),
     not(member(Value, Impossible)),
-    (Left = Value, Right = Value
+    (Left = Value, Right = Value, length(Possible, PossibleLength), PossibleLength = 0
     	->	push_level, level(NextLevel),
     		always_true_on(NextLevel), write("[ALWAYS TRUE] ")
     	;	(Left =< Value, Value =< Right; member(Value, Possible)),
@@ -115,6 +140,34 @@ router(Vertex) :-
     ),
     copy_vars_to_next_level_except(Level, NextLevel, Name),
     create_var(NextLevel, Name, Value, Value, [], []).
+
+router(Vertex) :-
+    statement(Vertex, LName, Operation, RName),
+    Operation = "EQV",
+    level(Level), 
+    var(Level, LName, LLeft, LRight, LPossible, LImpossible),
+    var(Level, RName, RLeft, RRight, RPossible, RImpossible),
+    remove(LPossible, LImpossible, LFilteredPossible),
+    remove(RPossible, RImpossible, RFilteredPossible),
+    not(neqv(Level, LName, RName)),
+    (LLeft = RLeft, LRight = RRight,
+     length(LPossible, LPossibleLength), LPossibleLength = 0,
+     length(RPossible, RPossibleLength), RPossibleLength = 0 
+        ->  push_level, level(NextLevel),
+            always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+        ;   (LRight >= RLeft, RRight >= LLeft ; somev(LFilteredPossible, =, RFilteredPossible)),
+            push_level, level(NextLevel)
+    ),
+    create_eqv(NextLevel, LName, RName),
+    copy_vars_to_next_level_except2(Level, NextLevel, LName, RName),
+    (LLeft \= -inf -> LNewLeft  is max(LLeft, RLeft)   ; LNewLeft  is LLeft),
+    (LRight \= inf -> LNewRight is min(LRight, RRight) ; LNewRight  is LRight),
+    (RLeft \= -inf -> RNewLeft  is max(LLeft, RLeft)   ; RNewLeft is RLeft),
+    (RRight \= inf -> RNewRight is min(LRight, RRight) ; RNewRight is RRight),
+    include(<(LNewLeft), LPossible, LTempPossible), include(>(LNewRight), LTempPossible, LNewPossible),
+    include(<(RNewLeft), RPossible, RTempPossible), include(>(RNewRight), RTempPossible, RNewPossible),
+    create_var(NextLevel, LName, LNewLeft, LRight, LNewPossible, LImpossible),
+    (LName \= RName -> create_var(NextLevel, RName, RLeft, RNewRight, RNewPossible, RImpossible) ; true).
 
 router(Vertex) :-
     statement(Vertex, Name, Operation, Value), 
@@ -134,6 +187,40 @@ router(Vertex) :-
     create_var(NextLevel, Name, Left, Right, Possible, UpdatedImpossible).
 
 router(Vertex) :-
+    statement(Vertex, LName, Operation, RName),
+    Operation = "NEQV",
+    level(Level), 
+    var(Level, LName, LLeft, LRight, LPossible, LImpossible),
+    var(Level, RName, RLeft, RRight, RPossible, RImpossible),
+    remove(LPossible, LImpossible, LFilteredPossible),
+    remove(RPossible, RImpossible, RFilteredPossible),
+    not(eqv(Level, LName, RName)),
+    LName \= RName,
+    (
+    	((RLeft > LRight) ;
+         (LLeft > RRight) ;
+    	 (LLeft = LRight, RLeft = RRight, (LLeft \= RLeft ; allneq(LFilteredPossible, RFilteredPossible)), write("here3")) ;
+         (range_list(LLeft, LRight, LRange), alleq(LRange, RImpossible)) ;
+         (range_list(RLeft, RRight, RRange), alleq(RRange, LImpossible))
+        )
+    	->  push_level, level(NextLevel),
+    		always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+    	;   (LRight > RLeft, RRight > LLeft ; 
+                (LLeft = LRight, RLeft = RRight, LLeft = RLeft, 
+                (
+                 length(LFilteredPossible, LLength), LLength \= 0 ;
+                 length(RFilteredPossible, RLength), RLength \= 0
+                )),
+                somev(LFilteredPossible, \=, LLeft) ; somev(RFilteredPossible, \=, LLeft)
+            ),
+            push_level, level(NextLevel)
+    ),
+    create_neqv(NextLevel, LName, RName),
+    copy_vars_to_next_level_except2(Level, NextLevel, LName, RName),
+    create_var(NextLevel, LName, LLeft, LRight, LPossible, LImpossible),
+    (LName \= RName -> create_var(NextLevel, RName, RLeft, RRight, RPossible, RImpossible) ; true).
+
+router(Vertex) :-
     statement(Vertex, Name, Operation, Value),
     Operation = "GT",
     level(Level), var(Level, Name, Left, Right, Possible, Impossible),
@@ -148,19 +235,31 @@ router(Vertex) :-
     create_var(NextLevel, Name, NewLeft, Right, Possible, Impossible).
 
 router(Vertex) :-
+    statement(Vertex, Name, Operation, Value),
+    Operation = "GEQ",
+    level(Level), var(Level, Name, Left, Right, Possible, Impossible),
+    (Left >= Value
+    	->	push_level, level(NextLevel),
+    		always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+    	;	(Right >= Value ; remove(Possible, Impossible, FilteredPossible), somev(FilteredPossible, >=, Value)),
+    		push_level, level(NextLevel)
+    ),
+    copy_vars_to_next_level_except(Level, NextLevel, Name),
+    (Left = Right -> NewLeft is Left ; NewLeft is Value),
+    create_var(NextLevel, Name, NewLeft, Right, Possible, Impossible).
+
+router(Vertex) :-
     statement(Vertex, LName, Operation, RName),
     Operation = "GTV",
     level(Level), 
     var(Level, LName, LLeft, LRight, LPossible, LImpossible),
-    write(var(Level, LName, LLeft, LRight, LPossible, LImpossible)),
     var(Level, RName, RLeft, RRight, RPossible, RImpossible),
-    write(var(Level, RName, RLeft, RRight, RPossible, RImpossible)),
     remove(LPossible, LImpossible, LFilteredPossible),
     remove(RPossible, RImpossible, RFilteredPossible),
     (LLeft > RRight
         ->  push_level, level(NextLevel),
             always_true_on(NextLevel), write("[ALWAYS TRUE] ")
-        ;   (LRight >= RLeft ; somev(LFilteredPossible, >, RFilteredPossible)),
+        ;   (LRight > RLeft ; somev(LFilteredPossible, >, RFilteredPossible)),
             push_level, level(NextLevel)
     ),
     copy_vars_to_next_level_except2(Level, NextLevel, LName, RName),
@@ -169,7 +268,29 @@ router(Vertex) :-
     include(<(LNewLeft), LPossible, LNewPossible),
     include(>(RNewRight), RPossible, RNewPossible),
     create_var(NextLevel, LName, LNewLeft, LRight, LNewPossible, LImpossible),
-    create_var(NextLevel, RName, RLeft, RNewRight, RNewPossible, RImpossible).
+    ( LName \= RName -> create_var(NextLevel, RName, RLeft, RNewRight, RNewPossible, RImpossible) ; true).
+
+router(Vertex) :-
+    statement(Vertex, LName, Operation, RName),
+    Operation = "GEQV",
+    level(Level), 
+    var(Level, LName, LLeft, LRight, LPossible, LImpossible),
+    var(Level, RName, RLeft, RRight, RPossible, RImpossible),
+    remove(LPossible, LImpossible, LFilteredPossible),
+    remove(RPossible, RImpossible, RFilteredPossible),
+    (LLeft >= RRight
+        ->  push_level, level(NextLevel),
+            always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+        ;   (LRight >= RLeft ; somev(LFilteredPossible, >=, RFilteredPossible)),
+            push_level, level(NextLevel)
+    ),
+    copy_vars_to_next_level_except2(Level, NextLevel, LName, RName),
+    (RLeft \= inf -> LNewLeft  is max(LLeft, RLeft)   ; LNewLeft  is LLeft),
+    (LRight \= -inf -> RNewRight is min(LRight, RRight) ; RNewRight is RRight),
+    include(=<(LNewLeft), LPossible, LNewPossible),
+    include(>=(RNewRight), RPossible, RNewPossible),
+    create_var(NextLevel, LName, LNewLeft, LRight, LNewPossible, LImpossible),
+    (LName \= RName -> create_var(NextLevel, RName, RLeft, RNewRight, RNewPossible, RImpossible) ; true).
 
 router(Vertex) :-
     statement(Vertex, Name, Operation, Value),
@@ -186,6 +307,64 @@ router(Vertex) :-
     create_var(NextLevel, Name, Left, NewRight, Possible, Impossible).
 
 router(Vertex) :-
+    statement(Vertex, Name, Operation, Value),
+    Operation = "LEQ",
+    level(Level), var(Level, Name, Left, Right, Possible, Impossible),
+    (Right =< Value
+    	->	push_level, level(NextLevel),
+    		always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+    	;	(Left =< Value ; remove(Possible, Impossible, FilteredPossible), somev(FilteredPossible, =<, Value)),
+    		push_level, level(NextLevel)
+    ),
+    copy_vars_to_next_level_except(Level, NextLevel, Name),
+    (Left = Right -> NewRight is Right ; NewRight is Value),
+    create_var(NextLevel, Name, Left, NewRight, Possible, Impossible).
+
+router(Vertex) :-
+    statement(Vertex, LName, Operation, RName),
+    Operation = "LTV",
+    level(Level), 
+    var(Level, LName, LLeft, LRight, LPossible, LImpossible),
+    var(Level, RName, RLeft, RRight, RPossible, RImpossible),
+    remove(LPossible, LImpossible, LFilteredPossible),
+    remove(RPossible, RImpossible, RFilteredPossible),
+    (LRight < RLeft
+        ->  push_level, level(NextLevel),
+            always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+        ;   (LLeft < RRight ; somev(LFilteredPossible, <, RFilteredPossible)),
+            push_level, level(NextLevel)
+    ),
+    copy_vars_to_next_level_except2(Level, NextLevel, LName, RName),
+    (RRight \= inf -> LNewRight is min(LRight, RRight - 1)   ; LNewRight  is LRight),
+    (LLeft \= -inf -> RNewLeft is max(RLeft, LLeft + 1) ; RNewLeft is RLeft),
+    include(>(LNewRight), LPossible, LNewPossible),
+    include(<(RNewLeft), RPossible, RNewPossible),
+    create_var(NextLevel, LName, LLeft, LNewRight, LNewPossible, LImpossible),
+    (LName \= RName -> create_var(NextLevel, RName, RNewLeft, RRight, RNewPossible, RImpossible) ; true).
+
+router(Vertex) :-
+    statement(Vertex, LName, Operation, RName),
+    Operation = "LEQV",
+    level(Level), 
+    var(Level, LName, LLeft, LRight, LPossible, LImpossible),
+    var(Level, RName, RLeft, RRight, RPossible, RImpossible),
+    remove(LPossible, LImpossible, LFilteredPossible),
+    remove(RPossible, RImpossible, RFilteredPossible),
+    (LRight =< LLeft
+        ->  push_level, level(NextLevel),
+            always_true_on(NextLevel), write("[ALWAYS TRUE] ")
+        ;   (LLeft >= RRight ; somev(LFilteredPossible, =<, RFilteredPossible)),
+            push_level, level(NextLevel)
+    ),
+    copy_vars_to_next_level_except2(Level, NextLevel, LName, RName),
+    (RRight \= inf -> LNewRight is min(LRight, RRight)   ; LNewRight  is LRight),
+    (LLeft \= -inf -> RNewLeft is max(RLeft, LLeft) ; RNewLeft is RLeft),
+    include(>=(LNewRight), LPossible, LNewPossible),
+    include(=<(RNewLeft), RPossible, RNewPossible),
+    create_var(NextLevel, LName, LLeft, LNewRight, LNewPossible, LImpossible),
+    (LName \= RName -> create_var(NextLevel, RName, RNewLeft, RRight, RNewPossible, RImpossible) ; true).
+
+router(Vertex) :-
     statement(Vertex, _Name, Operation, _Value),
     Operation = "ENDIF",
     level(Level), pop_level, level(PrevLevel),
@@ -194,7 +373,9 @@ router(Vertex) :-
     	;	(PrevLevel \= 0 -> copy_possibles_to_prev_level(Level, PrevLevel); true)
     ),
     always_true_off(Level),
-    delete_vars(Level).
+    delete_vars(Level),
+    delete_eqvs(Level),
+    delete_neqvs(Level).
 
 router(Vertex) :-
     statement(Vertex, _Name, Operation, _Value),
